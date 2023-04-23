ab8d630e98e697b6748f4fa83f3ccda1da7f831d
SamuelFontes <samugueldias@gmail.com>
2023-04-23 02:53:04 -0300

    Billing microservice logging on order creation

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
+import { RmqModule } from '@app/common';
+import * as Joi from 'joi';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
-import { RmqModule } from '@app/common';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    validationSchema: Joi.object({
-      RABBIT_MQ_URI: Joi.string().required,
-      RABBIT_MQ_BILLING_QUEUE: Joi.string().required,
-    })
-  }),
-    RmqModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+    }),
+    RmqModule,
+  ],
   controllers: [BillingController],
   providers: [BillingService],
 })
